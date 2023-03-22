c7c2434210e6e013fca092e712bdd45240f23669
Loi Than Trong <thantrongloi648@gmail.com>
2023-03-22 20:19:26 +0700

    path image

--- js/dataFake.js
+++ js/dataFake.js
@@ -2,37 +2,37 @@
 const portfolio_list = [
   {
     id: 1,
-    image : './assets/portfolio1.png',
+    image : '../assets/portfolio1.png',
     title : 'Green & Tasty Lemon',
     category : 'Fruits'
   },
   {
     id: 2,
-    image : './assets/portfolio2.png',
+    image : '../assets/portfolio2.png',
     title : 'Organic Carrot',
     category : 'Farmer'
   },
   {
     id: 3,
-    image : './assets/portfolio3.png',
+    image : '../assets/portfolio3.png',
     title : 'Organic Betel Leaf',
     category : 'Leaf'
   },
   {
     id: 4,
-    image : './assets/portfolio4.png',
+    image : '../assets/portfolio4.png',
     title : 'Natural Tommato',
     category : 'Fruits'
   },
   {
     id: 5,
-    image : './assets/portfolio5.png',
+    image : '../assets/portfolio5.png',
     title : 'Black Raspberry',
     category : 'Farmer'
   },
   {
     id: 6,
-    image : './assets/portfolio6.png',
+    image : '../assets/portfolio6.png',
     title : 'Honey Orange',
     category : 'Farmer'
   },
@@ -42,7 +42,7 @@ const card_list = [
     {
       id: 1,
       category: "Vegetable",
-      image: "./assets/calabrese.png",
+      image: "../assets/calabrese.png",
       title: "Calabrese Broccoli",
       price: "$20.00",
       sale: "$13.00",
@@ -51,7 +51,7 @@ const card_list = [
     {
       id: 2,
       category: "Fresh",
-      image: "./assets/banana.png",
+      image: "../assets/banana.png",
       title: "Fresh Banana Fruites",
       price: "$20.00",
       sale: "$14.00",
@@ -60,7 +60,7 @@ const card_list = [
     {
       id: 3,
       category: "Millets",
-      image: "./assets/white-nuts.png",
+      image: "../assets/white-nuts.png",
       title: "White Nuts",
       price: "$20.00",
       sale: "$15.00",
@@ -69,7 +69,7 @@ const card_list = [
     {
       id: 4,
       category: "Vegetable",
-      image: "./assets/vegan.png",
+      image: "../assets/vegan.png",
       title: "Vegan Red Tomato",
       price: "$20.00",
       sale: "$17.00",
@@ -78,7 +78,7 @@ const card_list = [
     {
       id: 5,
       category: "Health",
-      image: "./assets/mung-bean.png",
+      image: "../assets/mung-bean.png",
       title: "Mung Bean",
       price: "$20.00",
       sale: "$11.00",
@@ -87,7 +87,7 @@ const card_list = [
     {
       id: 6,
       category: "Nuts",
-      image: "./assets/hazelnut.png",
+      image: "../assets/hazelnut.png",
       title: "Brown Hazelnut",
       price: "$20.00",
       sale: "$12.00",
@@ -96,7 +96,7 @@ const card_list = [
     {
       id: 7,
       category: "Fresh",
-      image: "./assets/eggs.png",
+      image: "../assets/eggs.png",
       title: "Eggs",
       price: "$20.00",
       sale: "$17.00",
@@ -105,7 +105,7 @@ const card_list = [
     {
       id: 8,
       category: "Fresh",
-      image: "./assets/zelco.png",
+      image: "../assets/zelco.png",
       title: "Zelco Suji Elaichi Rusk",
       price: "$20.00",
       sale: "$15.00",
@@ -114,7 +114,7 @@ const card_list = [
     {
       id: 9,
       category: "Health",
-      image: "./assets/mung.png",
+      image: "../assets/mung.png",
       title: "Mung Bean",
       price: "$20.00",
       sale: "$11.00",
@@ -123,7 +123,7 @@ const card_list = [
     {
       id: 10,
       category: "Nuts",
-      image: "./assets/white-nuts.png",
+      image: "../assets/white-nuts.png",
       title: "White Hazelnut",
       price: "$20.00",
       sale: "$12.00",
@@ -132,7 +132,7 @@ const card_list = [
     {
       id: 11,
       category: "Fresh",
-      image: "./assets/corn.png",
+      image: "../assets/corn.png",
       title: "Fresh Corn",
       price: "$20.00",
       sale: "$17.00",
@@ -141,7 +141,7 @@ const card_list = [
     {
       id: 12,
       category: "Fresh",
-      image: "./assets/almonds.png",
+      image: "../assets/almonds.png",
       title: "Organic Almonds",
       price: "$20.00",
       sale: "$15.00",
@@ -150,7 +150,7 @@ const card_list = [
     {
       id: 13,
       category: "Vegetable",
-      image: "./assets/mung-bean2.png",
+      image: "../assets/mung-bean2.png",
       title: "Mung Bean",
       price: "$20.00",
       sale: "$11.00",
@@ -159,7 +159,7 @@ const card_list = [
     {
       id: 14,
       category: "Vegetable",
-      image: "./assets/brown-hazelnut.png",
+      image: "../assets/brown-hazelnut.png",
       title: "Brown Hazelnut",
       price: "$20.00",
       sale: "$12.00",
@@ -168,7 +168,7 @@ const card_list = [
     {
       id: 15,
       category: "Vegetable",
-      image: "./assets/onion.png",
+      image: "../assets/onion.png",
       title: "Onion",
       price: "$20.00",
       sale: "$17.00",
@@ -177,7 +177,7 @@ const card_list = [
     {
       id: 16,
       category: "Vegetable",
-      image: "./assets/cabbage.png",
+      image: "../assets/cabbage.png",
       title: "Cabbage",
       price: "$20.00",
       sale: "$15.00",
