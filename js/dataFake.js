b65413d92d94b367fbeec1aba569bb6fcd944c9e
Loi Than Trong <thantrongloi648@gmail.com>
2023-03-23 15:25:31 +0700

    datafake

--- js/dataFake.js
+++ js/dataFake.js
@@ -2,37 +2,37 @@
 const portfolio_list = [
   {
     id: 1,
-    image: require('../assets/portfolio1.png'),
+    image: ('../assets/portfolio1.png'),
     title: 'Green & Tasty Lemon',
     category: 'Fruits'
   },
   {
     id: 2,
-    image: require('../assets/portfolio2.png'),
+    image: ('../assets/portfolio2.png'),
     title: 'Organic Carrot',
     category: 'Farmer'
   },
   {
     id: 3,
-    image: require('../assets/portfolio3.png'),
+    image: ('../assets/portfolio3.png'),
     title: 'Organic Betel Leaf',
     category: 'Leaf'
   },
   {
     id: 4,
-    image: require('../assets/portfolio4.png'),
+    image: ('../assets/portfolio4.png'),
     title: 'Natural Tommato',
     category: 'Fruits'
   },
   {
     id: 5,
-    image: require('../assets/portfolio5.png'),
+    image: ('../assets/portfolio5.png'),
     title: 'Black Raspberry',
     category: 'Farmer'
   },
   {
     id: 6,
-    image: require('../assets/portfolio6.png'),
+    image: ('../assets/portfolio6.png'),
     title: 'Honey Orange',
     category: 'Farmer'
   },
@@ -42,7 +42,7 @@ const card_list = [
   {
     id: 1,
     category: "Vegetable",
-    image: require("../assets/calabrese.png"),
+    image: ("../assets/calabrese.png"),
     title: "Calabrese Broccoli",
     price: "$20.00",
     sale: "$13.00",
@@ -51,7 +51,7 @@ const card_list = [
   {
     id: 2,
     category: "Fresh",
-    image: require("../assets/banana.png"),
+    image: ("../assets/banana.png"),
     title: "Fresh Banana Fruites",
     price: "$20.00",
     sale: "$14.00",
@@ -60,7 +60,7 @@ const card_list = [
   {
     id: 3,
     category: "Millets",
-    image: require("../assets/white-nuts.png"),
+    image: ("../assets/white-nuts.png"),
     title: "White Nuts",
     price: "$20.00",
     sale: "$15.00",
@@ -69,7 +69,7 @@ const card_list = [
   {
     id: 4,
     category: "Vegetable",
-    image: require("../assets/vegan.png"),
+    image: ("../assets/vegan.png"),
     title: "Vegan Red Tomato",
     price: "$20.00",
     sale: "$17.00",
@@ -78,7 +78,7 @@ const card_list = [
   {
     id: 5,
     category: "Health",
-    image: require("../assets/mung-bean.png"),
+    image: ("../assets/mung-bean.png"),
     title: "Mung Bean",
     price: "$20.00",
     sale: "$11.00",
@@ -87,7 +87,7 @@ const card_list = [
   {
     id: 6,
     category: "Nuts",
-    image: require("../assets/hazelnut.png"),
+    image: ("../assets/hazelnut.png"),
     title: "Brown Hazelnut",
     price: "$20.00",
     sale: "$12.00",
@@ -96,7 +96,7 @@ const card_list = [
   {
     id: 7,
     category: "Fresh",
-    image: require("../assets/eggs.png"),
+    image: ("../assets/eggs.png"),
     title: "Eggs",
     price: "$20.00",
     sale: "$17.00",
@@ -105,7 +105,7 @@ const card_list = [
   {
     id: 8,
     category: "Fresh",
-    image: require("../assets/zelco.png"),
+    image: ("../assets/zelco.png"),
     title: "Zelco Suji Elaichi Rusk",
     price: "$20.00",
     sale: "$15.00",
@@ -114,7 +114,7 @@ const card_list = [
   {
     id: 9,
     category: "Health",
-    image: require("../assets/mung.png"),
+    image: ("../assets/mung.png"),
     title: "Mung Bean",
     price: "$20.00",
     sale: "$11.00",
@@ -123,7 +123,7 @@ const card_list = [
   {
     id: 10,
     category: "Nuts",
-    image: require("../assets/white-nuts.png"),
+    image: ("../assets/white-nuts.png"),
     title: "White Hazelnut",
     price: "$20.00",
     sale: "$12.00",
@@ -132,7 +132,7 @@ const card_list = [
   {
     id: 11,
     category: "Fresh",
-    image: require("../assets/corn.png"),
+    image: ("../assets/corn.png"),
     title: "Fresh Corn",
     price: "$20.00",
     sale: "$17.00",
@@ -141,7 +141,7 @@ const card_list = [
   {
     id: 12,
     category: "Fresh",
-    image: require("../assets/almonds.png"),
+    image: ("../assets/almonds.png"),
     title: "Organic Almonds",
     price: "$20.00",
     sale: "$15.00",
@@ -150,7 +150,7 @@ const card_list = [
   {
     id: 13,
     category: "Vegetable",
-    image: require("../assets/mung-bean2.png"),
+    image: ("../assets/mung-bean2.png"),
     title: "Mung Bean",
     price: "$20.00",
     sale: "$11.00",
@@ -159,16 +159,16 @@ const card_list = [
   {
     id: 14,
     category: "Vegetable",
-    image: require("../assets/brown-hazelnut.png"),
+    image: ("../assets/brown-hazelnut.png"),
     title: "Brown Hazelnut",
     price: "$20.00",
     sale: "$12.00",
-    star: 5,
+    star: 5,  
   },
   {
     id: 15,
     category: "Vegetable",
-    image: require("../assets/onion.png"),
+    image: ("../assets/onion.png"),
     title: "Onion",
     price: "$20.00",
     sale: "$17.00",
@@ -177,7 +177,7 @@ const card_list = [
   {
     id: 16,
     category: "Vegetable",
-    image: require("../assets/cabbage.png"),
+    image: ("../assets/cabbage.png"),
     title: "Cabbage",
     price: "$20.00",
     sale: "$15.00",
@@ -188,7 +188,7 @@ const card_list = [
 const team = [
   {
     id: 1,
-    image: require('../assets/team1.png'),
+    image: ('../assets/team1.png'),
     name: 'Giovani Bacardo',
     profession: 'Farmer',
     social: {
@@ -199,7 +199,7 @@ const team = [
   },
   {
     id: 2,
-    image: require('../assets/team2.png'),
+    image: ('../assets/team2.png'),
     name: 'Marianne Loreno',
     profession: 'Designer',
     social: {
@@ -210,7 +210,7 @@ const team = [
   },
   {
     id: 3,
-    image: require('../assets/team3.png'),
+    image: ('../assets/team3.png'),
     name: 'Riga Pelore',
     profession: 'Farmer',
     social: {
@@ -221,7 +221,7 @@ const team = [
   },
   {
     id: 4,
-    image: require('../assets/team4.png'),
+    image: ('../assets/team4.png'),
     name: 'Keira Knightley',
     profession: 'Farmer',
     social: {
@@ -232,7 +232,7 @@ const team = [
   },
   {
     id: 5,
-    image: require('../assets/team5.png'),
+    image: ('../assets/team5.png'),
     name: 'Scott Lawrence',
     profession: 'Designer',
     social: {
@@ -243,7 +243,7 @@ const team = [
   },
   {
     id: 6,
-    image: require('../assets/team6.png'),
+    image: ('../assets/team6.png'),
     name: 'Karen Allen',
     profession: 'Farmer',
     social: {
@@ -256,7 +256,7 @@ const team = [
 const blog = [
   {
     id: 1,
-    image: require('../assets/blog1.png'),
+    image: ('../assets/blog1.png'),
     date: '25-Nov',
     admin: 'Rachi Card',
     title: 'The Benefits of Vitamin D & How to Get It',
@@ -264,7 +264,7 @@ const blog = [
   },
   {
     id: 2,
-    image: require('../assets/blog2.png'),
+    image: ('../assets/blog2.png'),
     date: '25-Nov',
     admin: 'Rachi Card',
     title: 'Our Favorite Summertime Tomato',
@@ -272,7 +272,7 @@ const blog = [
   },
   {
     id: 1,
-    image: require('../assets/blog3.png'),
+    image: ('../assets/blog3.png'),
     date: '25-Nov',
     admin: 'Rachi Card',
     title: 'Benefits of Vitamin C & How to Get It',
@@ -280,7 +280,7 @@ const blog = [
   },
   {
     id: 2,
-    image: require('../assets/blog4.png'),
+    image: ('../assets/blog4.png'),
     date: '25-Nov',
     admin: 'Rachi Card',
     title: 'Research More Organic Foods',
@@ -288,7 +288,7 @@ const blog = [
   },
   {
     id: 1,
-    image: require('../assets/blog5.png'),
+    image: ('../assets/blog5.png'),
     date: '25-Nov',
     admin: 'Rachi Card',
     title: 'Everyday Fresh Fruites',
@@ -296,7 +296,7 @@ const blog = [
   },
   {
     id: 2,
-    image: require('../assets/blog6.png'),
+    image: ('../assets/blog6.png'),
     date: '25-Nov',
     admin: 'Rachi Card',
     title: 'Don’t Use Plastic Product! it’s Kill Nature',
